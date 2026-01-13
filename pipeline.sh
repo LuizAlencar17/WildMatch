af946d723578b2ae4ad9b8b4842be33dcf275218
Luiz Fabio <fabio.alencar644@gmail.com>
2026-01-13 15:17:30 -0400

    update

--- pipeline.sh
+++ pipeline.sh
@@ -6,9 +6,9 @@
 # python main.py --dataset "caltech" --image_type "cropped"
 # python main.py --dataset "wcs" --image_type "cropped"
 
-python main_clip_fusion.py --dataset "serengeti" --image_type "full"
-python main_clip_fusion.py --dataset "caltech" --image_type "full"
-python main_clip_fusion.py --dataset "wcs" --image_type "full"
+# python main_clip_fusion.py --dataset "serengeti" --image_type "full"
+# python main_clip_fusion.py --dataset "caltech" --image_type "full"
+# python main_clip_fusion.py --dataset "wcs" --image_type "full"
 
 # python main.py --dataset "serengeti" --image_type "full"
 # python main.py --dataset "caltech" --image_type "full"
